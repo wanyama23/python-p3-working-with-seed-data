04fd5ca737818e9e45b5e494b8763e3e98f15c07
edmond <edmond.wanyama@student.moringaschool.com>
2023-09-05 11:46:42 +0300

    completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -14,6 +14,29 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    session.query(Game).delete()
+    session.commit()
 
 
-    import ipdb; ipdb.set_trace()
+botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+ccs = Game(title="Candy Crush Saga", platform="Mobile", genre="Puzzle", price=0)
+
+session.add_all([botw, ffvii, mk8, ccs])
+session.commit()
+
+
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    )
+for i in range(50)]
+
+session.add_all(games)
+session.commit()
+
+import ipdb; ipdb.set_trace()
